b452f5e08595f6a2c6a2eb69b67f29c8ccb7f041
Stefan Dehelean <stefan.dehelean@aqvangarde-software.com>
2023-05-29 14:51:23 +0300

    updates

--- index.js
+++ index.js
@@ -1,19 +1,61 @@
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+var cookieParser = require('cookie-parser')
+
+const PORT = process.env.PORT || 5001
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser())
+// app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  // res.header('Access-Control-Allow-Origin', '*');
+  // res.header(
+  //   'Access-Control-Allow-Methods',
+  //   'POST, GET, PUT, DELETE, OPTIONS'
+  // );
+  // res.header(
+  //   'Access-Control-Allow-Headers',
+  //   'Origin, X-Requested-With, Content-Type, Accept, x-access-token'
+  // );
 
-app.post('/account', (req, res) => {
-  const accToken = req.cookies.accToken; // Read the "accToken" cookie
+  if (req.method === 'OPTIONS') {
+    res.end();
+  } else {
+    next();
+  }
+});
+
+app.post('/graphql', (req, res) => {
+  const accToken = req?.cookies?.accToken ?? 'asdasdasdas'; // Read the "accToken" cookie
+  console.log(accToken)
 
   // Handle the account POST request
   // ...
 
-  res.send('Account request received');
+  res.status(200).send({"data": {}});
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.listen(PORT, () => {
+  console.log('Server listening on port ' + PORT);
 });
+
+// var app = express()
+
+//
+
+// app.use(express.static(path.join(__dirname, 'public')))
+// app.set('views', path.join(__dirname, 'views'))
+// app.set('view engine', 'ejs')
+// app.get('/', (req, res) => res.render('pages/index'))
+// app.post('/graphql', cors(corsOptions), (req, res) => {
+//     console.log(req.cookies)
+//     // const accToken = req.cookies?.accToken; // Read the "accToken" cookie
+  
+//     // Handle the account POST request
+//     // ...
+  
+//     res.send('Account request received');
+//   })
+// app.listen(PORT, () => console.log(`Listening on ${ PORT } yeah`))
